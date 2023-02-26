9a7ef06648edc0b0449929defd7dff33d8427c52
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-25 20:31:07 -0500

    Bounded Queue

---
+++ BoundedQueue.js
@@ -0,0 +1,42 @@
+const LinkedList = require('./LinkedList');
+
+class Queue {
+  constructor(maxSize = Infinity) {
+    this.queue = new LinkedList();
+    this.size = 0;
+    this.maxSize = maxSize;
+  }
+
+  hasRoom() {
+    if (this.size < this.maxSize) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  isEmpty() {
+    if (this.size === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+  }
+
+  dequeue() {
+    const data = this.queue.removeHead();
+    this.size--;
+    console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+    return data;
+  }
+}
+
+module.exports = Queue;
