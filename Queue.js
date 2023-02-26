b83d5b4724a9c4aeca887bae04431e5a85cf5f84
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-25 20:17:58 -0500

    dequeue

--- Queue.js
+++ Queue.js
@@ -7,16 +7,27 @@ class Queue {
   }
 
   enqueue(data) {
-    this.queue.addToTail(data)
+    this.queue.addToTail(data);
     this.size++;
-    console.log(`Added ${data}! Queue size is now ${this.size}.`)
-    }
+    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+  }
+
+  dequeue() {
+    const data = this.queue.removeHead();
+    this.size--;
+    console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+     return data;
+  }
 }
 
 module.exports = Queue;
 
 const restaurantOrder = new Queue();
-console.log(`restaurantOrder queue has ${restaurantOrder.size} orders.\n`);
 restaurantOrder.enqueue('apple pie');
 restaurantOrder.enqueue('roast chicken');
 restaurantOrder.enqueue('quinoa salad');
+console.log('\nFood preparing...\n')
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+console.log('All orders ready!')
